ed432840fdf9c78d8c0ee22db2ebdddfef07047e
Bishop1988git config --global user.name Bishop1988git config --global user.name Bishop1988 <michaelabishop1988@gmail.com>
2021-08-10 10:47:11 +0100

    First Commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import AvailalbeMeals from './AvailableMeals'
+import MealsSummary from './MealsSummary'
+
+const Meals = () => {
+    return (
+        <>
+            <MealsSummary /> 
+            <AvailalbeMeals /> 
+        </>
+    )
+}
+
+export default Meals
